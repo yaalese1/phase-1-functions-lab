3c8753f8574530e16bbddfa34ddd9812eb2a7bfb
Yetunde Alese <yaalese1@gmail.com>
2022-11-01 10:51:44 -0400

    commit tree e7a2dcc2166ac2f070dc51b006cc15ad5a8a16e0

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(currentLocation){
+    return Math.abs( 42 - currentLocation)
+}
+
+function distanceFromHqInFeet (blocksInFeet){
+   
+    return (distanceFromHqInBlocks (blocksInFeet) * 264)
+}
+
+
+
+function distanceTravelledInFeet(startingPoint, endingPoint ){
+    
+    
+
+    return Math.abs (startingPoint - endingPoint) * 264
+}
+
+
+
+        
